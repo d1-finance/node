e55743c5dc19907ddf708e2ed38b8fc8889def3e
benjyz <ben@vegaswap.io>
2022-01-23 15:00:33 +0700

    websocket

--- go.mod
+++ go.mod
@@ -6,6 +6,7 @@ require (
 	github.com/aws/aws-sdk-go v1.29.16
 	github.com/btcsuite/btcd v0.22.0-beta
 	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/gorilla/websocket v1.4.2 // indirect
 	github.com/pkg/errors v0.9.1
 	github.com/polygonledger/edn v0.0.3
 )
---
+++ httpserver.go
@@ -0,0 +1,107 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"strings"
+
+	"github.com/gorilla/websocket"
+)
+
+func homePage(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "<h1>Horizon ledger</h1>")
+}
+
+func wsEndpoint(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Websockets")
+}
+
+func setupRoutes() {
+	http.HandleFunc("/", homePage)
+	http.HandleFunc("/ws", wsEndpoint)
+}
+
+// func main() {
+// 	fmt.Println("start webserver")
+// 	setupRoutes()
+// 	log.Fatal(http.ListenAndServe(":8080", nil))
+// }
+
+var upgrader = websocket.Upgrader{}
+var todoList []string
+
+func getCmd(input string) string {
+	inputArr := strings.Split(input, " ")
+	return inputArr[0]
+}
+
+func getMessage(input string) string {
+	inputArr := strings.Split(input, " ")
+	var result string
+	for i := 1; i < len(inputArr); i++ {
+		result += inputArr[i]
+	}
+	return result
+}
+
+func updateTodoList(input string) {
+	tmpList := todoList
+	todoList = []string{}
+	for _, val := range tmpList {
+		if val == input {
+			continue
+		}
+		todoList = append(todoList, val)
+	}
+}
+
+func main() {
+
+	http.HandleFunc("/todo", func(w http.ResponseWriter, r *http.Request) {
+		// Upgrade upgrades the HTTP server connection to the WebSocket protocol.
+		conn, err := upgrader.Upgrade(w, r, nil)
+		if err != nil {
+			log.Print("upgrade failed: ", err)
+			return
+		}
+		defer conn.Close()
+
+		// Continuosly read and write message
+		for {
+			mt, message, err := conn.ReadMessage()
+			if err != nil {
+				log.Println("read failed:", err)
+				break
+			}
+			input := string(message)
+			//cmd := getCmd(input)
+			//msg := getMessage(input)
+			msg := input
+			todoList = append(todoList, msg)
+			// if cmd == "add" {
+			// 	todoList = append(todoList, msg)
+			// }
+			// else if cmd == "done" {
+			// 	updateTodoList(msg)
+			// }
+			output := "Current Log: \n"
+			for _, todo := range todoList {
+				output += "\n - " + todo + "\n"
+			}
+			output += "\n----------------------------------------"
+			message = []byte(output)
+			err = conn.WriteMessage(mt, message)
+			if err != nil {
+				log.Println("write failed:", err)
+				break
+			}
+		}
+	})
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		http.ServeFile(w, r, "websockets.html")
+	})
+
+	http.ListenAndServe(":8080", nil)
+}
